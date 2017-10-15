1d323010b5f256ff751cbc607498a10d9df584e9
krish-thorcode <krish.thorcode@gmail.com>
2017-10-15 12:45:06 +0530

    added hello world label!

--- first_window/first_window.c
+++ first_window/first_window.c
@@ -5,8 +5,13 @@ int main(int argc,char *argv[])
 	gtk_init(&argc,&argv); //initialise gtk first of all
 
 	GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL); //toplevel window
+
+	GtkWidget *label = gtk_label_new("Hello, world!");
+
+	gtk_container_add(GTK_CONTAINER(window),label);
+
 	g_signal_connect(window,"delete_event",G_CALLBACK(gtk_main_quit),NULL); //delete_event can be replaced by delete-event
-	gtk_widget_show(window);
+	gtk_widget_show_all(window);
 
 	gtk_main();
 	return 0;
