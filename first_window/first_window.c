ec3ebae6ecb44835f08b0eaf004eb7b99bdef1c4
krish-thorcode <krish.thorcode@gmail.com>
2017-10-15 11:37:40 +0530

    delete_event also worked. Deleted the executable file by using make clean

--- first_window/first_window.c
+++ first_window/first_window.c
@@ -5,7 +5,7 @@ int main(int argc,char *argv[])
 	gtk_init(&argc,&argv); //initialise gtk first of all
 
 	GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL); //toplevel window
-	g_signal_connect(window,"delete-event",G_CALLBACK(gtk_main_quit),NULL);
+	g_signal_connect(window,"delete_event",G_CALLBACK(gtk_main_quit),NULL); //delete_event can be replaced by delete-event
 	gtk_widget_show(window);
 
 	gtk_main();
