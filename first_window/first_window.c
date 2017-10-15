4472ebf0e2a31695ca54814d0c36a4ba1fa3da86
krish-thorcode <krish.thorcode@gmail.com>
2017-10-15 11:26:30 +0530

    g_signal_connect() worked (the c++guy used g_signal_connect() in his video, while in the book GTK+ Gnome Application gtk_signal_connect has been used)

--- first_window/first_window.c
+++ first_window/first_window.c
@@ -5,7 +5,7 @@ int main(int argc,char *argv[])
 	gtk_init(&argc,&argv); //initialise gtk first of all
 
 	GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL); //toplevel window
-	gtk_signal_connect(window,"close_window",G_CALLBACK(gtk_main_quit),NULL);
+	g_signal_connect(window,"close_window",G_CALLBACK(gtk_main_quit),NULL);
 	gtk_widget_show(window);
 
 	gtk_main();
