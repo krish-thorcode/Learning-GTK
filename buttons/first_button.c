a5f478f21003be0ea119714b8c426c82cd7cb8b0
krish-thorcode <krish.thorcode@gmail.com>
2017-10-15 14:23:35 +0530

    empty button

---
+++ buttons/first_button.c
@@ -0,0 +1,18 @@
+#include<gtk/gtk.h>
+
+int main(int argc,char *argv[])
+{
+	gtk_init(&argc,&argv);
+
+	GtkWidget *window,*button;
+	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	button = gtk_button_new();
+
+	gtk_container_add(GTK_CONTAINER(window),button);
+	g_signal_connect(window,"delete-event",(G_CALLBACK(gtk_main_quit)),NULL);
+
+	gtk_widget_show_all(window);
+
+	gtk_main();
+	return 0;
+}
