e873bbe5733c5ad048b669f685b9cd9c8aac9d18
krish-thorcode <krish.thorcode@gmail.com>
2017-10-15 11:19:48 +0530

    undefined reference to gtk_ functions despite having added the gtk.h header file and also referencing to the pkg files during compilation using the Makefile

---
+++ first_window/first_window.c
@@ -0,0 +1,13 @@
+#include<gtk/gtk.h>
+
+int main(int argc,char *argv[])
+{
+	gtk_init(&argc,&argv); //initialise gtk first of all
+
+	GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL); //toplevel window
+	gtk_signal_connect(window,"close_window",G_CALLBACK(gtk_main_quit),NULL);
+	gtk_widget_show(window);
+
+	gtk_main();
+	return 0;
+}
