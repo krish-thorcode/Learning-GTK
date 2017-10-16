c782ab608cb9046ba125f63c4642ae66a4a35fa3
krish-thorcode <krish.thorcode@gmail.com>
2017-10-16 19:15:36 +0530

    tried adding gtk stock button. segmentation fault.

---
+++ buttons/default_button_from_stock.c
@@ -0,0 +1,20 @@
+#include<gtk/gtk.h>
+
+int main(int argc,char *argv[])
+{
+	gtk_init(&argc,&argv);
+
+	GtkWidget *window, *button;
+
+	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	g_signal_connect(window,"delete-event",G_CALLBACK(gtk_main_quit),NULL);
+
+	button =gtk_button_new_from_stock(GTK_STOCK_APPLY);
+
+	gtk_container_add(GTK_CONTAINER(window),window);
+
+	gtk_widget_show_all(window);
+
+	gtk_main();
+	return 0;
+}
